660ac6d5eff8f81fac90bff847932c478fb8589f
Eclips4 <kirill.bast9@mail.ru>
2022-07-27 00:30:26 +0300

    Initialize tests, and add .gitignore

--- simple_config/config.py
+++ simple_config/config.py
@@ -1,13 +1,23 @@
+from typing import List
+
+
 class SimpleConfig:
     def __init__(self) -> None:
         self.data = {}
 
+    def update_data(self, field: List[str]) -> None:
+        key = field[0]
+        value = field[1]
+        prepared_key = key.strip()
+        prepared_value = value.strip().split("\\n")[0]
+        self.data.update({prepared_key: prepared_value})
+
     def parse(self, path: str) -> None:
         with open(path) as file:
             data = file.readlines()
-            for i in data:
-                index = i.split(':')
-                self.data.update({index[0]: index[1].replace('\n', '').split("//")[0]})
+            for field in data:
+                field_ = field.split('=')
+                self.update_data(field_)
 
     def get(self, item: str) -> str:
         return self.data[item]
---
+++ tests/test_config.py
@@ -0,0 +1,18 @@
+from unittest import TestCase, main
+from simple_config import SimpleConfig
+
+
+PATH_TO_TEST_CONFIG = './test.smpl'
+
+
+class TestConfig(TestCase):
+    def test_config(self):
+        config = SimpleConfig()
+        config.parse(PATH_TO_TEST_CONFIG)
+        self.assertEqual(
+            "value", config.get("key")
+        )
+
+
+if __name__ == '__main__':
+    main()

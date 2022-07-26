b9a78f1c6837a19d47657067661837a8aba9f7b6
DoomGod <70023601+elezor-dev@users.noreply.github.com>
2022-07-27 00:53:42 +0500

    Тайпхинтов много не бывает.
    зачем что то говорить если и так все написанно?

--- simpleconfig.py
+++ simpleconfig.py
@@ -3,7 +3,7 @@ class SimpleConfig:
 	def __init__(self):
 		self.data = {}
 
-	def parse(self, file: str):
+	def parse(self, file: str) -> None:
 		file = open(file, 'r')
 		data = file.readlines()
 		
@@ -15,6 +15,6 @@ class SimpleConfig:
 
 		file.close()
 
-	def get(self, what: str):
+	def get(self, what: str) -> str:
 
 		return self.data[what]

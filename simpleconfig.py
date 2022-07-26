5b44bec2f56fa1fbdb87e90b64668f1a1a58bb00
DoomGod <70023601+elezor-dev@users.noreply.github.com>
2022-07-27 00:51:17 +0500

    Uploaded "simpleconfig.py"
    Впринципе логично...

---
+++ simpleconfig.py
@@ -0,0 +1,20 @@
+
+class EasyConfig:
+	def __init__(self):
+		self.data = {}
+
+	def parse(self, file: str):
+		file = open(file, 'r')
+		data = file.readlines()
+		
+		for i in data:
+			
+			index = i.split(':')
+
+			self.data.update({index[0]: index[1].replace('\n', '').split("//")[0]})
+
+		file.close()
+
+	def get(self, what: str):
+
+		return self.data[what]
